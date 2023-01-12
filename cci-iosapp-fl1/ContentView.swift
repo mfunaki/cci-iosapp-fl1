2b9d7ba47b7530dc30032ff5147b6a42cf964934
Masahiko Funaki <mfunaki@mayoct.net>
2023-01-12 10:14:46 +0800

    first commit

---
+++ cci-iosapp-fl1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  cci-iosapp-fl1
+//
+//  Created by Masahiko Funaki on 2023/01/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cci-iosapp-fl1/cci_iosapp_fl1App.swift
@@ -0,0 +1,17 @@
+//
+//  cci_iosapp_fl1App.swift
+//  cci-iosapp-fl1
+//
+//  Created by Masahiko Funaki on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct cci_iosapp_fl1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
